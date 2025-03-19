3940fbb08193a158ac7e7b66203cb7935947f9cb
Poussin144p <johnathan.besse@outlook.fr>
2025-03-19 15:19:08 +0100

    Je suis pas front moi non plus

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,10 @@ export function Header({ setPage }) {
     return (
         <header className="bg-neutral-800 text-white pb-4 mb-10">
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold hover:cursor-pointer" onClick={() => setPage('events')}>Evenemondial</h1>
-                <ShoppingBasketIcon className="text-white position-right" onClick={() => setPage('checkout')} />
+                <h1 className="text-2xl font-bold hover:cursor-pointer" onClick={() => setPage('events')}>
+                    Evenemondial
+                </h1>
+                <ShoppingBasketIcon className="text-white cursor-pointer" onClick={() => setPage('checkout')} />
             </div>
         </header>
     );
